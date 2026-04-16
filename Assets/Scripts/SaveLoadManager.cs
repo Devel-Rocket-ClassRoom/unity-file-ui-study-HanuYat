f77304ba302b6ed567f9a5244a636b7439a0ca10
HanuYat <jnjn1235@gmail.com>
2026-04-16 17:33:54 +0900

    AES 및 UI 시스템 따라하기 및 실습

--- Assets/Scripts/SaveLoadManager.cs
+++ Assets/Scripts/SaveLoadManager.cs
@@ -1,26 +1,28 @@
-using UnityEngine;
-using System.IO;
 using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
 using SaveDataVC = SaveDataV3;
 
 public static class SaveLoadManager
 {
     public enum SaveMode
     {
-        Text,
-        Encrypted
+        Text, // .json
+        Encrypted // .dat
     }
 
-    public static SaveMode Mode { get; set; } = SaveMode.Text;
+    public static SaveMode Mode { get; set; } = SaveMode.Encrypted;
     public static int SaveDataVersion { get; } = 3;
+
+    public static byte[] Encrypted;
 
     private static readonly string SaveDirectory = Path.Combine(Application.persistentDataPath, "Save");
     private static readonly string[] SaveFileNames =
     {
-        "SaveAuto.json",
-        "Save1.json",
-        "Save2.json",
-        "Save3.json"
+        "SaveAuto",
+        "Save1",
+        "Save2",
+        "Save3"
     };
 
     public static SaveDataVC Data { get; set; } = new SaveDataVC();
@@ -32,6 +34,11 @@ public static class SaveLoadManager
     };
 
     public static bool Save(int slot = 0)
+    {
+        return Save(slot, Mode);
+    }
+
+    public static bool Save(int slot, SaveMode mode)
     {
         if (Data == null || slot < 0 || slot >= SaveFileNames.Length)
         {
@@ -45,9 +52,20 @@ public static class SaveLoadManager
                 Directory.CreateDirectory(SaveDirectory);
             }
 
-            var path = Path.Combine(SaveDirectory, SaveFileNames[slot]);
+            var path = GetSavePath(slot);
             var json = JsonConvert.SerializeObject(Data, settings);
-            File.WriteAllText(path, json);
+            switch (Mode)
+            {
+                case SaveMode.Text:
+                    File.WriteAllText(path, json);
+                    break;
+
+                case SaveMode.Encrypted:
+                    File.WriteAllBytes(path, CryptoUtil.Encrypt(json));
+                    break;
+            }
+
+            Debug.Log(path);
             return true;
         }
         catch
@@ -59,12 +77,17 @@ public static class SaveLoadManager
 
     public static bool Load(int slot = 0)
     {
+        return Load(slot, Mode);
+    }
+
+    public static bool Load(int slot, SaveMode mode)
+    {
         if (slot < 0 || slot >= SaveFileNames.Length)
         {
             return false;
         }
 
-        var path = Path.Combine(SaveDirectory, SaveFileNames[slot]);
+        var path = GetSavePath(slot);
         if (!File.Exists(path))
         {
             return false;
@@ -72,7 +95,18 @@ public static class SaveLoadManager
 
         try
         {
-            var json = File.ReadAllText(path);
+            var json = string.Empty;
+            switch (Mode)
+            {
+                case SaveMode.Text:
+                    json = File.ReadAllText(path);
+                    break;
+
+                case SaveMode.Encrypted:
+                    json = CryptoUtil.Decrypt(File.ReadAllBytes(path));
+                    break;
+            }
+
             var saveData = JsonConvert.DeserializeObject<SaveData>(json, settings);
             while (saveData.Version < SaveDataVersion)
             {
@@ -89,5 +123,17 @@ public static class SaveLoadManager
             Debug.LogError("Load 예외");
             return false;
         }
+    }
+
+    public static string GetSavePath(SaveMode mode, int slot)
+    {
+        var ext = mode == SaveMode.Text ? ".json" : ".dat";
+
+        return Path.Combine(SaveDirectory, $"{SaveFileNames[slot]}{ext}");
+    }
+
+    private static string GetSavePath(int slot)
+    {
+        return GetSavePath(Mode, slot);
     }
 }
